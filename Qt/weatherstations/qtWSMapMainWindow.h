daf8f1cc57753ae3424b5b377e5b831db05d5749
John Tourtellott <john.tourtellott@kitware.com>
2014-08-29 09:52:54 -0400

    Add VTK Headers to source files

--- Qt/weatherstations/qtWSMapMainWindow.h
+++ Qt/weatherstations/qtWSMapMainWindow.h
@@ -1,13 +1,29 @@
-/*
-Main window for weather station map application
-*/
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkMap
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+   This software is distributed WITHOUT ANY WARRANTY; without even
+   the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+   PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME qtWSMapMainWindow - Main window for weather station map application
+// .SECTION Description
+//
 
 
-#ifndef _qtWSMapMainWindow_h_
-#define _qtWSMapMainWindow_h_
+#ifndef __qtWSMapMainWindow_h
+#define __qtWSMapMainWindow_h
 
 #include "vtkMap.h"
 #include <qmainwindow.h>
+
+class QWidget;
 
 
 class qtWSMapMainWindow : public QMainWindow
@@ -17,8 +33,10 @@ class qtWSMapMainWindow : public QMainWindow
 
   void drawMap();
  protected:
+  QWidget *createCoordinatesDisplay();
+
   vtkMap *Map;
 };
 
-#endif
+#endif  // __qtWSMapMainWindow_h
 
--- Qt/weatherstations/wsmap.cxx
+++ Qt/weatherstations/wsmap.cxx
@@ -1,6 +1,20 @@
-/*
-Weather station map using vtkMap
-*/
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkMap
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+   This software is distributed WITHOUT ANY WARRANTY; without even
+   the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+   PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME wsmap.cxx - Weather station map demo program
+// .SECTION Description
+//
 
 
 #include "qtWSMapMainWindow.h"
