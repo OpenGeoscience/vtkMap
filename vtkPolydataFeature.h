e9f684c4587b3ddd2ff3ff0e88c4ff60c26ee8a7
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-10-20 23:03:11 -0400

    Added geometry feature

---
+++ vtkPolydataFeature.h
@@ -0,0 +1,69 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+   This software is distributed WITHOUT ANY WARRANTY; without even
+   the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+   PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkPolydataFeature -
+// .SECTION Description
+//
+
+#ifndef __vtkPolydataFeature_h
+#define __vtkPolydataFeature_h
+
+#include <vtkActor.h>
+#include <vtkObject.h>
+#include <vtkPolyDataMapper.h>
+
+#include "vtkFeature.h"
+
+#include <string>
+
+class vtkPolydataFeature : public vtkFeature
+{
+public:
+  static vtkPolydataFeature* New();
+  virtual void PrintSelf(ostream &os, vtkIndent indent);
+  vtkTypeMacro(vtkPolydataFeature, vtkObject);
+
+  // Description
+  // Get actor for the polydata
+  vtkGetObjectMacro(Actor, vtkActor);
+
+  // Description
+  // Get mapper for the polydata
+  vtkGetObjectMacro(Mapper, vtkPolyDataMapper);
+
+  // Description:
+  // Override
+  virtual void Init();
+
+  // Description:
+  // Override
+  virtual void CleanUp();
+
+  // Description:
+  // Override
+  virtual void Update();
+
+protected:
+  vtkPolydataFeature();
+  ~vtkPolydataFeature();
+
+  vtkActor* Actor;
+  vtkPolyDataMapper* Mapper;
+
+private:
+  vtkPolydataFeature(const vtkPolydataFeature&); // Not implemented
+  void operator=(const vtkPolydataFeature&); // Not implemented
+};
+
+
+#endif // __vtkPolydataFeature_h
