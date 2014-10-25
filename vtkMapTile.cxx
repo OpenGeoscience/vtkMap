6588b6c4340c5bd2547e2e1518e8e18f8c01881a
John Tourtellott <john.tourtellott@kitware.com>
2014-10-25 18:47:06 -0400

    Check for curl error, and remove image file on error

--- vtkMapTile.cxx
+++ vtkMapTile.cxx
@@ -28,6 +28,7 @@
 
 #include <curl/curl.h>
 
+#include <cstdio>  // for remove()
 #include <sstream>
 #include <fstream>
 
@@ -172,6 +173,7 @@ void vtkMapTile::DownloadImage(const char *url, const char *outfilename)
   CURL* curl;
   FILE* fp;
   CURLcode res;
+  char errorBuffer[CURL_ERROR_SIZE];
   curl = curl_easy_init();
 
   if(curl)
@@ -183,12 +185,19 @@ void vtkMapTile::DownloadImage(const char *url, const char *outfilename)
       return;
       }
 
+    curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errorBuffer);
     curl_easy_setopt(curl, CURLOPT_URL, url);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
     res = curl_easy_perform(curl);
     curl_easy_cleanup(curl);
     fclose(fp);
+    }
+
+  if (res != CURLE_OK)
+    {
+    remove(outfilename);
+    vtkWarningMacro(<<errorBuffer);
     }
 }
 
