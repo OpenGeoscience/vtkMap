c66b54c5e716fc9ff33f4a87d6f8c4b445a1c0f6
John Tourtellott <john.tourtellott@kitware.com>
2014-10-31 13:03:46 -0400

    Add vtkMap::FindLayer() method.
    Also fix error in ny-points.geojson test file.
    Also update readme file to indicate the vtkDataSetReaders requirement.

--- vtkMap.h
+++ vtkMap.h
@@ -82,6 +82,7 @@ public:
   // Add / Remove layer from the map.
   void AddLayer(vtkLayer* layer);
   void RemoveLayer(vtkLayer* layer);
+  vtkLayer *FindLayer(char *name);
 
   // TODO Implement this
   //void SetLayerOrder(vtkLaye* layer, int offsetFromCurrent);
